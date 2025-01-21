00a908793e04beb4586038e4cf1f983fbeab3db3
Shashank Nigudgi <shashankan1si17me110@gmail.com>
2025-01-21 06:10:59 +0530

    Okay

--- Multitreading/src/multiThreads.java
+++ Multitreading/src/multiThreads.java
@@ -1,11 +1,17 @@
 public class multiThreads {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        //creating the for loop in order to understand the which thread the coplier is executing
+        // and surprisingly the random threads will be selected and started.
         for(int i = 0; i<=3; i ++)
         {
             Books b1 = new Books(i);
             b1.start();
+            b1.join();//this is used to kill the threads, basically after first thread it will run one by one.
+
         }
+        //throw  new RuntimeException("Pls check it is failed ");
+        ///even if it is a run time exception , threads will not stop.
     }
 }
 
@@ -29,6 +35,13 @@ class Books extends Thread {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+
+            //let us create a run time exception in threads and see
+
+//            if(bookno == 3)
+//            {
+//                throw new RuntimeException();
+//            }
         }
     }
 }
